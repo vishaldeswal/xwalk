7ed495ac76c4f7cf1ddfc93114a2fa5467c0e39c
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-21 15:09:51 +0530

    Console log block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,9 @@
 export default function decorate(block) {
     const [quoteWrapper] = block.children;
+    console.log("BLOCK: \n",block, "\n");
   
     const blockquote = document.createElement('blockquote');
+    console.log("BLOCK CHILDREN: \n",quoteWrapper, "\n");
     blockquote.textContent = quoteWrapper.textContent.trim();
     quoteWrapper.replaceChildren(blockquote);
   }
