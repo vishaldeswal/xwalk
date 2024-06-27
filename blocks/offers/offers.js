99ebe0bf28228ace37d38d88dd327bf12b914a3b
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-27 12:01:48 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -133,7 +133,7 @@ function createTeaserCard(title, description, actionText, actionUrl, teaserClass
       actionDiv.appendChild(ctaClone);
     }
 
-    const leftContainerImage =extractElementFromBlock(blockChilren.card1, '[data-aue-prop="component_linkText"]');;
+    const leftContainerImage = extractElementFromBlock(blockChilren.card1, '[data-aue-prop="offer_BckImg"]');;
     console.log("Card Images:", leftContainerImage);
     // if (descriptionElement) {
     // contentDiv.appendChild(descriptionElement.cloneNode(true));
