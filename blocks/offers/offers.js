ed3e68556e03bacebef1481a9f1f6027ed73f43e
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-28 11:36:18 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -28,6 +28,40 @@ function createOffersHTML(component){
 
 function createPrimaryOffer(teaser){
 
+  let title = component.querySelector('[data-aue-prop="offer_Title"]');
+  let description = component.querySelector('[data-aue-prop="offer_Description"]');
+  let a = component.querySelector('[data-aue-prop="offer_linkText"]');
+  let bckImg = component.querySelector('[data-aue-prop="offer_BckImg"]');
+
+  if (a) {
+    a.classList.add("button","primary__btn");
+  }
+
+  const teaserHTML = `  <div class="left-container">
+                    <picture>
+                        <source srcset="./images/desktop-left-image.jpg" media="(min-width: 1024px)">
+                        ${bckImg? `${bckImg.outerHTML}`: ''}
+                    </picture>
+                    <div class="light-teaser buyers-guide-teaser">
+                        <div class="teaser__card teaser-grey">
+                            <div class="teaser__content">
+                                <div class="teaser__info left-img-over-text">
+                                    <div class="teaser__title">
+                                      ${title? `<h3>${title.innerText}</h3>`: ''}
+                                    </div>
+                                    <div class="teaser__description">
+                                        ${description? `<p>${description.innerText}</p>`:''}
+                                    </div>
+                                </div>
+                                <div class="teaser__action-btn">
+                                  ${a? a.outerHTML : ''}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+
+
 
   return teaserHTML;
 }
@@ -53,12 +87,16 @@ export default function decorate(block) {
   // console.log("Card 3: ", card3.outerHTML);
 
   const componentHTML = createOffersHTML(component);
+  const primaryCard = createPrimaryOffer(card1);
   
  
   block.innerHTML = `
   <section class="deals-offers-container">
         <div class="immersive__wrapper-light">
           ${componentHTML}
+        </div>
+        <div class="sub-container">
+          ${primaryCard}
         </div>
     </section>
   `;
