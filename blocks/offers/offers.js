f92e7ffc9a34a60caaf8729862cf9c3f09c40cc1
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-28 09:55:15 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -4,9 +4,9 @@
     
     const [component, card1, card2, card3, card4] = block.children;
 
-    console.log("Component: ", component);
-    console.log("Card 1: ", card1);
-    console.log("Card 3: ", card3);
+    console.log("Component: ", component.outerHTML);
+    console.log("Card 1: ", card1.outerHTML);
+    console.log("Card 3: ", card3.outerHTML);
   
   }
   
