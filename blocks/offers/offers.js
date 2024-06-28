4f432d9a2e61789b268c1be9b233f355bdf38ab0
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-28 11:52:57 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -93,17 +93,46 @@ export default function decorate(block) {
   block.innerHTML = `
   <section class="deals-offers-container">
         <div class="immersive__wrapper-light">
-          ${componentHTML}
-        </div>
-        <div class="sub-container">
-          ${primaryCard}
+            <div class="immersive__content heading-content">
 
-          <div class="right-container">
+                <h2>Exciting offers and deals for you!</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet.</p>
+                <div class="immersive__action-btn">
+                    <a class="btn--link btn--link-primary" href="#">View More
+                        <span>
+                            <img src="/content/dam/vishal_eds/north_east.svg" />
+                        </span></a>
+                </div> <!-- change -->
+            </div>
+            <div class="sub-container">
+                <div class="left-container">
+                    <picture>
+                        <source srcset="/content/dam/vishal_eds/desktop-left-image.jpg" media="(min-width: 1024px)">
+                        <img src="./content/dam/vishal_eds/desktop-left-image.jpg" alt="mobile img" />
+                    </picture>
+                    <div class="light-teaser buyers-guide-teaser">
+                        <div class="teaser__card teaser-grey">
+                            <div class="teaser__content">
+                                <div class="teaser__info left-img-over-text">
+                                    <div class="teaser__title">
+                                        <h3>Lorem Ipsum</h3>
+                                    </div>
+                                    <div class="teaser__description">
+                                        <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum.</p>
+                                    </div>
+                                </div>
+                                <div class="teaser__action-btn"><a href="#" title="#" class="button primary__btn"
+                                    target="_self">Get Exchange</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-container">
                     <div class="right-container__card-1">
                         <picture>
                             <source srcset="/content/dam/vishal_eds/mobile-image.png" media="(max-width: 768pxpx)">
                             <source srcset="/content/dam/vishal_eds/mobile-image.png" media="(min-width: 1024px)">
-                            <img src="/content/dam/vishal_eds/mobile-image.png" alt="mobile img" />
+                            <img src="./images/mobile-image.png" alt="mobile img" />
                         </picture>
                         <div class="right-container__content">
                             <div class="immersive__content img-over-text">
@@ -162,9 +191,10 @@ export default function decorate(block) {
                         </div>
                     </div>
                 </div>
-          
+            </div>
+        </div>              
         </div>
-      
+        </div>
     </section>
   `;
 }
