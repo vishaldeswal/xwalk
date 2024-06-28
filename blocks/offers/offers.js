e4290121df10dc080340574fd186143967e30617
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-28 12:04:13 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -40,7 +40,7 @@ function createPrimaryOffer(teaser) {
 
   const teaserHTML = `<div class="left-container">
    <picture>
-      <source srcset="./images/desktop-left-image.jpg" media="(min-width: 1024px)">
+      <source srcset="/content/dam/vishal_eds/desktop-image.png" media="(min-width: 1024px)">
       ${bckImg ? `${bckImg.outerHTML}` : ""}
    </picture>
    <div class="light-teaser buyers-guide-teaser">
