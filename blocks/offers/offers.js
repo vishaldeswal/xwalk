9cede07ded3e6202be772437bff680091327e992
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-28 13:27:47 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -17,7 +17,7 @@ function createOffersHTML(component) {
 
   const componentHTML = `
   <div class="immersive__content heading-content">
-  ${title ? `<h2>${title.innerText}</h2>` : ""}
+  ${title ? `${title.outerText}` : ""}
   ${description ? `<p>${description.innerText}</p>` : ""}
       <div class="immersive__action-btn">
           ${a ? a.outerHTML : ""}
@@ -56,7 +56,7 @@ leftContainer.innerHTML = `
          <div class="teaser__content">
             <div class="teaser__info left-img-over-text">
                <div class="teaser__title">
-                  ${title? `<h3>${title.innerText}</h3>` : ""}
+                  ${title? `${title.outerText}` : ""}
                </div>
                <div class="teaser__description">
                   ${description? `<p>${description.innerText}</p>`: ""}
@@ -100,7 +100,7 @@ function createSecondaryOffer(teaser) {
                         </picture>
                         <div class="right-container__content">
                             <div class="immersive__content img-over-text">
-                                ${title? `<p><strong>${title.innerText}</strong></p>` : ""}
+                                ${title? `${title.outerText}` : ""}
                                 ${description? `<p>${description.innerText}</p>`: ""}
                                 <div class="immersive__action">
                                  ${a ? a.outerHTML : ""}
@@ -113,8 +113,46 @@ function createSecondaryOffer(teaser) {
   return secCardContainer.outerHTML;
 }
 
-function createGeneralOffer(teaser) {
-  return teaserHTML;
+function createGeneralOffer(teaser, textClass) {
+  let title = teaser.querySelector('[data-aue-prop="offer_Title"]');
+  let description = teaser.querySelector('[data-aue-prop="offer_Description"]');
+  let a = teaser.querySelector('[data-aue-prop="offer_linkText"]');
+  let btnIcon = teaser.querySelector('[data-aue-prop="component_linkIcon"]');
+
+  if (a) {
+    a.classList.add("button", "primary__btn");
+  }
+
+  const genCardContainer = document.createElement('div');
+  genCardContainer.classList.add('right-container__card-2');
+
+  Array.from(teaser.attributes).forEach(attr => {
+    genCardContainer.setAttribute(attr.name, attr.value);
+  });
+
+  genCardContainer.innerHTML=`<div class="light-teaser buyers-guide-teaser">
+                                <div class="teaser__card ${textClass? textClass: ''}">
+                                    <div class="teaser__content">
+                                        <div class="teaser__info">
+                                            <div class="teaser__title">
+                                                 ${title? `${title.outerText}` : ""}
+                                            </div>
+                                            <div class="teaser__description">
+                                                 ${description? `<p>${description.innerText}</p>`: ""}
+                                            </div>
+                                        </div>
+                                        <div class="teaser__actions">
+                                          ${a ? a.outerHTML : ""}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+
+
+
+
+
+  return genCardContainer.outerHTML;
 }
 
 export default function decorate(block) {
@@ -127,6 +165,8 @@ export default function decorate(block) {
   const componentHTML = createOffersHTML(component);
   const primaryCard = createPrimaryOffer(card1);
   const secondaryCard= createSecondaryOffer(card2);
+  const generalCard1=createGeneralOffer(card3, 'teaser-light');
+  const generalCard2=createGeneralOffer(card4, 'teaser-dark');
 
   block.innerHTML = `
   <section class="deals-offers-container">
@@ -137,57 +177,12 @@ export default function decorate(block) {
                 <div class="right-container">
                     ${secondaryCard}
                     <div class="right-container-subcontainer ">
-                        <div class="right-container__card-2">
-                            <div class="light-teaser buyers-guide-teaser">
-                                <div class="teaser__card teaser-light">
-                                    <div class="teaser__content">
-                                        <div class="teaser__info">
-                                            <div class="teaser__title">
-                                                <h3>Lorem Ipsum</h3>
-                                            </div>
-                                            <div class="teaser__description">
-                                                <p>Lorem ipsum dolor sit amet lorem ipsum dolor.</p>
-                                            </div>
-                                        </div>
-
-                                        <div class="teaser__actions">
-                                            <a href="https://google.com" title="https://google.com"
-                                                class="button primary__btn" target="_blank">Know More</a>
-
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="right-container__card-3">
-                            <div class="light-teaser buyers-guide-teaser">
-                                <div class="teaser__card teaser-dark">
-                                    <div class="teaser__content">
-                                        <div class="teaser__info">
-                                            <div class="teaser__title">
-                                                <h3>Lorem Ipsum</h3>
-                                            </div>
-                                            <div class="teaser__description">
-                                                <p>Lorem ipsum dolor sit amet lorem ipsum dolor.</p>
-                                            </div>
-                                        </div>
-
-                                        <div class="teaser__actions">
-                                            <a href="https://google.com" title="https://google.com"
-                                                class="button primary__btn" target="_blank">Know More</a>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        ${generalCard1}
+                        ${generalCard2}
                     </div>
                 </div>
             </div>
         </div>              
-        </div>
-        </div>
     </section>
   `;
 }
