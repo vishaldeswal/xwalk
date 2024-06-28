a80d4d834097d446da1279caf65c36bab4ac1e0b
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-28 09:49:40 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -1,21 +1,8 @@
 
-  //Function to extract children of block
-  function extractBlockChildren(block){
-
-    const [component, card1, card2, card3, card4] = block.children;
-    return {
-      component,
-      card1,
-      card2,
-      card3,
-      card4
-    }
-  }
-
   // Main decorate function that orchestrates element extraction and structure building
   export default function decorate(block) {
     
-    [component, card1, card2, card3, card3] = extractBlockChildren(block);
+    [component, card1, card2, card3, card3] = block.children;
 
     console.log("Component: ", component);
     console.log("Card 1: ", card1);
