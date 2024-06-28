8062beac047ab5b8b9cb2b9549e231863f8ca5b2
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-06-28 11:21:56 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -26,12 +26,30 @@ function createOffersHTML(component){
 }
 
 
+function createPrimaryOffer(teaser){
+
+
+  return teaserHTML;
+}
+
+function createSecondaryOffer(teaser){
+
+  return teaserHTML;
+}
+
+function createGeneralOffer(teaser){
+  
+  
+  return teaserHTML;
+}
+
+
 
 export default function decorate(block) {
   const [component, card1, card2, card3, card4] = block.children;
 
   // console.log("Component: ", component.outerHTML);
-  // console.log("Card 1: ", card1.outerHTML);
+   console.log("Card 1: ", card1.outerHTML);
   // console.log("Card 3: ", card3.outerHTML);
 
   const componentHTML = createOffersHTML(component);
