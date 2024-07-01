fb84df80755e2cdf4989eb21bff0a9ee0c740653
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-07-01 15:49:56 +0530

    Update offers.js

--- blocks/offers/offers.js
+++ blocks/offers/offers.js
@@ -107,7 +107,7 @@ function createSecondaryOffer(teaser) {
 
   secCardContainer.innerHTML = `
                         <picture>
-                            <source media="(min-width: 1024px)" srcset="${bckImg ? `${bckImg.src}` : ""}" >
+                            <source media="(min-width: 769px)" srcset="${bckImg ? `${bckImg.src}` : ""}" >
                             <source media="(max-width: 768px)" srcset="${bckMobImg ? `${bckMobImg.src}` : ""}" >
                             <img src="/content/dam/vishal_eds/image.jpg" alt="Card1 Image Alternative" data-aue-prop="offer_BckImg" data-aue-label="Offer Background Image" data-aue-type="media">
                               
