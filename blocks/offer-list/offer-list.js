483e7a84142ad44b583d6c907b962339dc6beff9
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-07-05 17:24:55 +0530

    Update offer-list.js

--- blocks/offer-list/offer-list.js
+++ blocks/offer-list/offer-list.js
@@ -24,7 +24,7 @@ export default function decorate(block) {
       a.classList.remove("button");
       a.classList.add("btn--link", "btn--link-primary");
       if(target){
-        a.setAttribute('target', target.innerText);
+        a.setAttribute('target', target);
       }
       a.innerHTML = `${a.innerText} <span>${
         btnIcon ? `${btnIcon.outerHTML}` : ""
@@ -50,14 +50,55 @@ export default function decorate(block) {
 
 
     
-  block.innerHTML = `
-  <section class="deals-offers-container">
-        <div class="immersive__wrapper-light">
-            ${componentHTML}
-            
-        </div>              
-    </section>
-  `;
+ 
  
 }
 
+function createPrimaryOffer(card) {
+  
+  const [desktopImg, mobileImage, title, ] = card.children;
+
+
+  if (a) {
+    a.classList.add("button", "primary__btn");
+    if(target){
+      a.setAttribute('target', target.innerText);
+    }
+  }
+
+  const leftContainer = document.createElement('div');
+  leftContainer.classList.add('left-container');
+
+  Array.from(teaser.attributes).forEach(attr => {
+    leftContainer.setAttribute(attr.name, attr.value);
+});
+
+
+leftContainer.innerHTML = `
+   <picture>
+      <source srcset=" ${bckImg ? `${bckImg.src}` : ""}" media="(min-width: 1024px)">
+      ${bckImg ? `${bckImg.outerHTML}` : ""}
+   </picture>
+   <div class="light-teaser buyers-guide-teaser">
+      <div class="teaser__card teaser-grey">
+         <div class="teaser__content">
+            <div class="teaser__info left-img-over-text">
+               <div class="teaser__title">
+                  ${title? `${title.outerHTML}` : ""}
+               </div>
+               <div class="teaser__description">
+                  ${description? `<p>${description.innerText}</p>`: ""}
+               </div>
+            </div>
+            <div class="teaser__action-btn">
+               ${a ? a.outerHTML : ""}
+            </div>
+         </div>
+      </div>
+   </div>
+`;
+
+
+
+  return leftContainer.outerHTML;
+}
