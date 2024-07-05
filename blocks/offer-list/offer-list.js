2368d3f57f2f9b4236bdb13f8b8bec193945e2c8
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-07-05 17:52:11 +0530

    Update offer-list.js

--- blocks/offer-list/offer-list.js
+++ blocks/offer-list/offer-list.js
@@ -6,19 +6,15 @@ export default function decorate(block) {
 
     const [titleEL, typeEL, descriptionEL, ctaEl, ctaTargetEL, ctaIconEL, ...offerItems] = block.children;
 
-    console.log("Title:",titleEL);
-    console.log("Type:",typeEL);
-    console.log("description:",descriptionEL);
-    console.log("cta:",ctaEl.outerHTML);
-    console.log("ctaTarget:",ctaTargetEL);
-    console.log("ctaIcon:",ctaIconEL.outerHTML);
-
     const title = titleEL?.innerText?.trim() || '';
     const type = typeEL?.innerText?.trim() || '';
     const description = descriptionEL?.innerText?.trim() || '';
     const a = ctaEl?.querySelector('.button-container a') || '';
     const target = ctaTargetEL?.innerText?.trim() || '';
     const btnIcon = ctaIconEL?.querySelector('img') || '';
+
+    const primaryCard= createPrimaryOffer(offerItems[0]);
+    const secondaryCard= createSecondaryOffer(offerItems[1]);
 
     if (a) {
       a.classList.remove("button");
@@ -43,43 +39,81 @@ export default function decorate(block) {
   </div>
 `;
 
+
+block.innerHTML = `
+  <section class="deals-offers-container">
+        <div class="immersive__wrapper-light">
+            ${componentHTML}
+            <div class="sub-container">
+                ${primaryCard}
+                <div class="right-container">
+                    ${secondaryCard}
+                </div>
+            </div>
+        </div>              
+    </section>
+  `;
+
  
 }
 
 function createPrimaryOffer(card) {
   
-  const [desktopImg, mobileImage, title, ] = card.children;
+  const [desktopImgEl, mobileImageEl, titleEl, descriptionEl, ctaEl, ctaTargetEL  ] = card.children;
+
+  console.log("Desktop Img:",desktopImgEl);
+  console.log("Mobile Img:",mobileImageEl);
+  console.log("Title:",titleEl);
+  console.log("description:",descriptionEl);
+  console.log("cta:",ctaEl.outerHTML);
+  console.log("ctaTarget:",ctaTargetEL);
+
+
+  const desktopImgSrc = desktopImgEl?.querySelector('img')?.src || '';
+  const desktopImgAlt = desktopImgEl?.querySelector('img')?.alt || '';
+
+  const mobileImgSrc = mobileImageEl?.querySelector('img')?.src || '';
+  const mobileImgAlt = mobileImageEl?.querySelector('img')?.alt || '';
+  
+  const title = titleEl?.innerText?.trim() || '';
+  const description = descriptionEl?.innerText?.trim() || '';
+  
+  const a = ctaEl?.querySelector('.button-container a') || '';
+  const target = ctaTargetEL?.innerText?.trim() || '';
+  
+  
 
 
   if (a) {
     a.classList.add("button", "primary__btn");
     if(target){
-      a.setAttribute('target', target.innerText);
+      a.setAttribute('target', target);
     }
   }
 
   const leftContainer = document.createElement('div');
   leftContainer.classList.add('left-container');
 
-  Array.from(teaser.attributes).forEach(attr => {
+  Array.from(card.attributes).forEach(attr => {
     leftContainer.setAttribute(attr.name, attr.value);
 });
 
 
 leftContainer.innerHTML = `
    <picture>
-      <source srcset=" ${bckImg ? `${bckImg.src}` : ""}" media="(min-width: 1024px)">
-      ${bckImg ? `${bckImg.outerHTML}` : ""}
+      <source srcset=" ${desktopImgSrc ? `${desktopImgSrc}` : ""}" media="(min-width: 1024px)">
+      <source srcset=" ${mobileImgSrc ? `${mobileImgSrc}` : ""}" media="(max-width: 1024px)">
+      <img src="${desktopImgSrc ? `${desktopImgSrc}` : ""}" loading="lazy" alt="${desktopImgAlt}"/>
    </picture>
    <div class="light-teaser buyers-guide-teaser">
       <div class="teaser__card teaser-grey">
          <div class="teaser__content">
             <div class="teaser__info left-img-over-text">
                <div class="teaser__title">
-                  ${title? `${title.outerHTML}` : ""}
+                  ${title? `<p>${title}</p>` : ""}
                </div>
                <div class="teaser__description">
-                  ${description? `<p>${description.innerText}</p>`: ""}
+                  ${description? `<p>${description}</p>`: ""}
                </div>
             </div>
             <div class="teaser__action-btn">
@@ -93,4 +127,70 @@ leftContainer.innerHTML = `
 
 
   return leftContainer.outerHTML;
+}
+
+function createSecondaryOffer(card) {
+
+  const [desktopImgEl, mobileImageEl, titleEl, descriptionEl, ctaEl, ctaTargetEL  ] = card.children;
+
+  console.log("Desktop Img:",desktopImgEl);
+  console.log("Mobile Img:",mobileImageEl);
+  console.log("Title:",titleEl);
+  console.log("description:",descriptionEl);
+  console.log("cta:",ctaEl.outerHTML);
+  console.log("ctaTarget:",ctaTargetEL);
+
+
+  const desktopImgSrc = desktopImgEl?.querySelector('img')?.src || '';
+  const desktopImgAlt = desktopImgEl?.querySelector('img')?.alt || '';
+
+  const mobileImgSrc = mobileImageEl?.querySelector('img')?.src || '';
+  const mobileImgAlt = mobileImageEl?.querySelector('img')?.alt || '';
+  
+  const title = titleEl?.innerText?.trim() || '';
+  const description = descriptionEl?.innerText?.trim() || '';
+  
+  const a = ctaEl?.querySelector('.button-container a') || '';
+  const target = ctaTargetEL?.innerText?.trim() || '';
+  
+  
+
+  
+
+  if (a) {
+    a.classList.add("button", "primary__btn");
+    if(target){
+      a.setAttribute('target', target.innerText);
+    }
+    
+  }
+
+  const secCardContainer = document.createElement('div');
+  secCardContainer.classList.add('right-container__card-1');
+
+  Array.from(card.attributes).forEach(attr => {
+    secCardContainer.setAttribute(attr.name, attr.value);
+});
+
+  secCardContainer.innerHTML = `
+                        <picture class="image-container">
+                            <source media="(min-width: 769px)" srcset="${desktopImgSrc ? `${desktopImgSrc}` : ""}" >
+                            <source media="(max-width: 768px)" srcset="${mobileImgSrc ? `${mobileImgSrc}` : ""}" >
+                            <img src="${desktopImgSrc ? `${desktopImgSrc}` : ""}" loading="lazy" alt="${desktopImgAlt}"/>
+                              
+                        </picture>
+                        <div class="right-container__content">
+                            <div class="immersive__content img-over-text">
+                               <div class="teaser__title">${title? `<p>${title}</p>` : ""}</div>
+                               
+                                ${description? `<p>${description}</p>`: ""}
+                                <div class="immersive__action">
+                                 ${a ? a.outerHTML : ""}
+                                 </div>
+                            </div>
+                        </div>
+                    `;
+
+
+  return secCardContainer.outerHTML;
 }
